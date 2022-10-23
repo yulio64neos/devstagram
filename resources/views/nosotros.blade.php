db432be1a4c59384f1219f9c90f14b761c5b05ae
yulio64neos <julio64neos@gmail.com>
2022-10-23 00:35:57 -0500

    Estructura de la rama de archivos

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Nosotros
+@endsection
+
+@section('contenido')
+    Contenido de nosotros
+@endsection
---
+++ resources/views/tienda.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Tienda
+@endsection
+
+@section('contenido')
+    Contenido de la tienda
+@endsection
