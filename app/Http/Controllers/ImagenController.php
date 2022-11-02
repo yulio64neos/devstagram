8edf94ee8bb0fd8ea36b5e1c94aa097bd73bd587
yulio64neos <julio64neos@gmail.com>
2022-11-01 18:20:51 -0600

    Clase 100

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request){
         $imagen = $request->file('file');
 
-        return response()->json(['imagen' => $imagen->extension()]);
+        //Lo que hace es generar un id unico en cada una de las imágenes en el servidor
+        //No se pueden tener dos archivos iguales en el servidor
+        $nombreImagen = Str::uuid() . "." .$imagen->extension();
+
+        //La imágen que está en memoría será procesada en el servidor
+        //Esto ya forma una variable de intervention image
+        $imagenServidor = Image::make($imagen);
+        //El método FIT recorta la imagen para que se ajuste al tamaño que le pasemos
+        $imagenServidor->fit(1000, 1000);
+
+        //A mover la imágen en el servidor
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -12,5 +12,20 @@ const dropzone = new Dropzone("#dropzone", {
 });
 
 dropzone.on('sending', function(file, xhr, formData ){
-    console.log(file);
-})
+    console.log(formData);
+});
+
+dropzone.on('success', function(file, response){
+    console.log(response)
+});
+
+dropzone.on('error', function(file, message){
+    console.log(message)
+});
+
+dropzone.on('removedfile', function(){
+    console.log('Archivo borrado');
+});
+
+
+
